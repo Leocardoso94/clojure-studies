6836ca38e0070e740b1b2cc7df8c2f8adb624dd7
Leonardo Cardoso <leocardosoti@gmail.com>
2018-07-16 17:01:23 -0300

    using let

--- giggin/src/giggin/components/orders.cljs
+++ giggin/src/giggin/components/orders.cljs
@@ -5,20 +5,19 @@
 (defn get-gig-property-by-id [id]
   (fn [property] (get-in @state/gigs [id property])))
 
-(defn order-item [id quant]
-  (def get-gig-property
-    (get-gig-property-by-id id))
-  [:div.item {:key id}
-   [:div.img
-    [:img {:src (get-gig-property :img)
-           :alt (get-gig-property :id)}]]
-   [:div.content
-    [:p.title (str (get-gig-property :title) " \u00d7 " quant)]]
-   [:div.action
-    [:div.price (* (get-gig-property :price) quant)]
-    [:button.btn.btn--link.tooltip {:data-tooltip "Remove"
-                                    :on-click #(swap! state/orders dissoc id)}
-     [:i.icon.icon--cross]]]])
+(defn order-item [id quant remove-from-order]
+  (let [get-gig-property (get-gig-property-by-id id)]
+    [:div.item {:key id}
+     [:div.img
+      [:img {:src (get-gig-property :img)
+             :alt (get-gig-property :id)}]]
+     [:div.content
+      [:p.title (str (get-gig-property :title) " \u00d7 " quant)]]
+     [:div.action
+      [:div.price (* (get-gig-property :price) quant)]
+      [:button.btn.btn--link.tooltip {:data-tooltip "Remove"
+                                      :on-click #(remove-from-order id)}
+       [:i.icon.icon--cross]]]]))
 
 (defn total
   []
@@ -28,22 +27,24 @@
 
 
 (defn orders []
-  [:aside
-   (if (empty? @state/orders)
-     [:div.empty
-      [:div.title "You don't have any orders"]
-      [:div.subtitle "Click on + to add an order"]]
-     [:div.order
-      [:div.body
-       (for [[id quant] @state/orders]
-         (order-item id quant))]
-      [:div.total
-       [:hr]
-       [:div.item
-        [:div.content "Total"]
-        [:div.action
-         [:div.price (format-price (total))]]
-        [:button.btn.btn--link.tooltip
-         {:data-tooltip "Remove All"
-          :on-click #(reset! state/orders {})}
-         [:i.icon.icon--delete]]]]])])
+  (let [remove-from-order #(swap! state/orders dissoc %)
+        remove-all-orders #(reset! state/orders {})]
+    [:aside
+     (if (empty? @state/orders)
+       [:div.empty
+        [:div.title "You don't have any orders"]
+        [:div.subtitle "Click on + to add an order"]]
+       [:div.order
+        [:div.body
+         (for [[id quant] @state/orders]
+           (order-item id quant remove-from-order))]
+        [:div.total
+         [:hr]
+         [:div.item
+          [:div.content "Total"]
+          [:div.action
+           [:div.price (format-price (total))]]
+          [:button.btn.btn--link.tooltip
+           {:data-tooltip "Remove All"
+            :on-click #(remove-all-orders)}
+           [:i.icon.icon--delete]]]]])]))
